b6c5a8e058873c74d27ea8f20f5a9ed792dee969
AJ <alexanderjessediaz@gmail.com>
2021-03-01 18:59:34 -0700

    remove react preloaded data and styling,  install bootstrap, add a couple of bootstrap componenents and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {Jumbotron, Form } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Jumbotron>
+        <h1>Hello, world!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    </Jumbotron>
+        <Form>
+        <Form.Group>
+          <Form.Label>Form</Form.Label>
+          <Form.Control as="input" type="number"/>
+        </Form.Group>
+      </Form>
     </div>
-  );
+    );
 }
 
 export default App;
