0daa205fa8a6a922d1de7b61232fd487e4cfc5c2
AJ <alexanderjessediaz@gmail.com>
2021-03-01 19:34:37 -0700

    add time and date inputs, edit styling to look more organized, remove alert

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import {Alert, Button, Jumbotron, Form, Container, Row, Col } from 'react-bootstrap'
+import {Button, Jumbotron, Form, Container, Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <Container fluid="md">
 
         <Jumbotron>
           <h1>Did you walk the dog yet?</h1>
@@ -12,20 +11,34 @@ function App() {
           </p>
       </Jumbotron>
 
-      <Alert variant="success">
-        <Alert.Heading>Walk details</Alert.Heading>
-      </Alert>
+      <Container fluid="md">
 
         <Form>
           <Form.Group>
-            <Form.Label>How long of a walk in minutes?</Form.Label>
             <Row>
+              <Form.Label>What day did you walk?</Form.Label>
+              <Col xs={2}>
+                <Form.Control as="input" type="date" id="walkminutes"/>
+              </Col>
+            </Row>
+            <Row>
+              <Form.Label>What time did you walk?</Form.Label>
+              <Col xs={2}>
+              <Form.Control as="input" type="time" id="walkminutes"/>
+              </Col>
+            </Row>
+            <Row>
+            <Form.Label>How long of a walk in minutes?</Form.Label>
               <Col xs={1}>
                 <Form.Control as="input" type="number" id="walkminutes"/>
               </Col>
             </Row>
-            <Form.Check type="checkbox" label="Pee?" id="peecheck"/>
-            <Form.Check type="checkbox" label="Poop?" id="poopcheck"/>
+            <Row>
+              <Form.Check type="checkbox" label="Pee?" id="peecheck"/>
+            </Row>
+            <Row>
+              <Form.Check type="checkbox" label="Poop?" id="poopcheck"/>
+            </Row>
           </Form.Group>
           <Button variant="secondary" type="submit">
             Submit
